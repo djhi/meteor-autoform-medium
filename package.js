fd71eec93e7218abcb2b7245cd47e7304466b9de
djhi <gildas.garcia@gmail.com>
2015-03-28 15:55:33 +0100

    0.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'gildaspk:autoform-medium',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: "Medium editor for AutoForm",
   description: "Medium editor for AutoForm",
   git: "http://github.com/djhi/meteor-autoform-medium.git"
@@ -12,7 +12,7 @@ Package.onUse(function(api) {
   api.use([
     'coffeescript',
     'templating',
-    'aldeed:autoform@5.0.0',
+    'aldeed:autoform@5.1.1',
     'citizenkevin:medium-editor@1.0.0',
   ], 'client');
 
